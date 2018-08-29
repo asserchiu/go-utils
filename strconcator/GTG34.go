e8fa750157eebed2d7a153c2921d27ddf5dc0de8
Asser Chiu <asserchiu@gmail.com>
2018-08-29 10:32:37 +0800

    Improve tutorial codes

--- strconcator/GTG34.go
+++ strconcator/GTG34.go
@@ -1,22 +1,24 @@
 package strconcator
 
-// before
+import "strings"
 
-func GTG34(a, b string) (result string) {
-	result = a
-	for i := 0; i < 3; i++ {
-		result += b
-	}
-	return result
-}
-
-//// after
+//// before
 //
 //func GTG34(a, b string) (result string) {
-//	var s strings.Builder
-//	s.WriteString(a)
+//	result = a
 //	for i := 0; i < 3; i++ {
-//		s.WriteString(b)
+//		result += b
 //	}
-//	return s.String()
+//	return result
 //}
+
+// after
+
+func GTG34(a, b string) (result string) {
+	var s strings.Builder
+	s.WriteString(a)
+	for i := 0; i < 3; i++ {
+		s.WriteString(b)
+	}
+	return s.String()
+}
